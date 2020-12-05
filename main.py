6a746a3292abcc795cf92a1afb78a091ebe2a947
dspiljak <dominik.spiljak@reversinglabs.com>
2020-12-05 23:27:28 +0100

    Finished remaining tasks

---
+++ main.py
@@ -0,0 +1,119 @@
+import os
+import cv2 as cv
+import numpy as np
+import utils.image_processing as utils
+import matplotlib.pyplot as plt
+
+# Silence TensorFlow log
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+
+class Stack:
+    _stack: list
+
+    def __init__(self):
+        self._stack = []
+
+    def push(self, element):
+        self._stack.append(element)
+
+    def pop(self):
+        return self._stack.pop()
+
+    def peek(self):
+        return self._stack[-1]
+
+
+def hasPrecedence(operator1, operator2):
+    if operator2 == '(' or operator2 == ')':
+        return False
+    elif (operator1 == '*' or operator1 == '/') and (operator2 == '+' or operator2 == '-'):
+        return False
+    else:
+        return True
+
+
+def evaluate_expression(expression):
+    operator_map = {
+        '+': lambda x, y: x + y,
+        '-': lambda x, y: x - y,
+        '/': lambda x, y: x / y,
+        '*': lambda x, y: x * y
+    }
+    operator_stack = Stack()
+    value_stack = Stack()
+    current_number = ""
+    for i in range(len(expression)):
+        if expression[i] == '(':
+            operator_stack.push(expression[i])
+
+        if expression[i].isnumeric():
+            current_number += expression[i]
+
+        if not expression[i].isnumeric() and current_number != "":
+            value_stack.push(int(current_number))
+            current_number = ""
+
+        if expression[i] == ')':
+            while operator_stack.peek() != '(':
+                value2 = value_stack.pop()
+                value1 = value_stack.pop()
+                value_stack.push(operator_map[operator_stack.pop()](
+                    value1, value2))
+
+            operator_stack.pop()
+
+        if expression[i] in operator_map:
+            while operator_stack._stack != [] and hasPrecedence(expression[i], operator_stack.peek()):
+                value2 = value_stack.pop()
+                value1 = value_stack.pop()
+                value_stack.push(operator_map[operator_stack.pop()](
+                    value1, value2))
+            operator_stack.push(expression[i])
+
+    if current_number != "":
+        value_stack.push(int(current_number))
+        current_number = ""
+
+    while operator_stack._stack != []:
+        value2 = value_stack.pop()
+        value1 = value_stack.pop()
+        value_stack.push(operator_map[operator_stack.pop()](
+            value1, value2))
+
+    return value_stack.pop()
+
+
+def main():
+    from keras.models import load_model
+    model = load_model("models/lenet_25epochs.h5")
+
+    img = utils.load_and_process_image('test.jpeg')
+    bounding_boxes = sorted(
+        utils.find_unique_contours(img), key=lambda x: x[0])
+    cropped_boxes = [utils.crop_bounding_box(
+        img, bounding) for bounding in bounding_boxes]
+
+    X = np.array([cv.resize(cropped, (30, 30)).reshape(30, 30, 1)
+                  for cropped in cropped_boxes])
+
+    preds = np.argmax(model.predict(X), axis=1)
+
+    inverse_label_map = {
+        10: '+',
+        11: '-',
+        12: 'x',
+        13: '/',
+        14: '(',
+        15: ')'
+    }
+
+    expression = "".join([str(pred) if pred < 10 else inverse_label_map[pred]
+                          for pred in preds])
+
+    print('Decoded expression: {}'.format(expression))
+    print('Calculated expression: {}'.format(evaluate_expression(expression)))
+
+
+if __name__ == "__main__":
+    main()
---
+++ utils/image_processing.py
@@ -0,0 +1,76 @@
+import cv2 as cv
+import numpy as np
+
+
+def load_and_process_image(path):
+    img = cv.imread(path)
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    img_invert = cv.bitwise_not(img_gray)
+    return img_invert
+
+
+def find_contours(image):
+    ret, thresh = cv.threshold(image, 127, 255, 0)
+    return cv.findContours(
+        thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+
+def overlapping_axes(coord1, delta1, coord2, delta2):
+    if coord1 <= coord2 + delta2 and coord1 >= coord2:
+        return True
+    if coord1 + delta1 <= coord2 + delta2 and coord1 + delta1 >= coord2:
+        return True
+    if coord2 <= coord1 + delta1 and coord2 >= coord1:
+        return True
+    if coord2 + delta2 <= coord1 + delta1 and coord2 + delta2 >= coord1:
+        return True
+
+    return False
+
+
+def find_unique_contours(image):
+    contours, hierarchy = find_contours(image)
+    boundingRects = [cv.boundingRect(contour) for contour in contours]
+
+    # If 2 bounding boxes are overlapping, take the bigger one
+    for i in range(len(boundingRects)):
+        if boundingRects[i] is None:
+            continue
+        for j in range(i + 1, len(boundingRects)):
+            if boundingRects[j] is None:
+                continue
+            x1, y1, width1, height1 = boundingRects[i]
+            x2, y2, width2, height2 = boundingRects[j]
+
+            if overlapping_axes(x1, width1, x2, width2) and overlapping_axes(y1, height1, y2, height2):
+                if width1 * height1 > width2 * height2:
+                    boundingRects[j] = None
+                else:
+                    boundingRects[i] = None
+                    break
+
+    return [bounding for bounding in boundingRects if bounding is not None]
+
+
+def crop_bounding_box(image, bounding_box):
+    x, y, width, height = bounding_box
+    cropped_image = image[y:y+height, x:x+width]
+    return cropped_image
+
+
+def vectorize(path):
+    img = load_and_process_image(path)
+    contours, _ = find_contours(img)
+    # In case of multiple contours, take one with the biggest area
+    bounding_box = cv.boundingRect(sorted(contours, key=cv.contourArea)[-1])
+    img = crop_bounding_box(img, bounding_box)
+    img = cv.resize(img, (30, 30))
+    img = img.reshape(30, 30, 1)
+    return img
+
+
+if __name__ == "__main__":
+    x1, y1, width1, height1 = (62, 59, 135, 363)
+    x2, y2, width2, height2 = (181, 76, 3, 3)
+    print(overlapping_axes(x1, width1, x2, width2),
+          overlapping_axes(y1, height1, y2, height2))
